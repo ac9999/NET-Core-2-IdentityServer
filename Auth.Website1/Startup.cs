3ed80a17a486a95504c95cdc8f750ecdf5f602d3
Amit Chandel <amitchandel@zoho.com>
2019-04-02 09:56:41 -0400

    added multiple website authentication for identity server 4

---
+++ Auth.Website1/Startup.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.DependencyInjection;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Auth.Website1
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options => {
+                options.Filters.Add(new RequireHttpsAttribute());
+            });
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            services.AddAuthentication(options => {
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+                .AddOpenIdConnect(options => {
+                    options.Authority = "https://localhost:44371";
+                    options.ClientId = "Website1";
+                    options.SaveTokens = true;
+                    options.TokenValidationParameters.NameClaimType = "name";
+                })
+                .AddCookie();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRewriter(new RewriteOptions().AddRedirectToHttps(301, 5001));
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# PoC_IdentityServer
+
+Proof of concept for Identity Server 4 authorization and authentication.
